6b74bd0b7f281ab8903a46d2fec132f33e041afd
zoker <wuzk@gz-mstc.com>
2017-12-01 15:28:18 +0800

    初始化项目

---
+++ app/src/main/java/zoker/net/ucache/app/MainActivity.java
@@ -0,0 +1,13 @@
+package zoker.net.ucache.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
